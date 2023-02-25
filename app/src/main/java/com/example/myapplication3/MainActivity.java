59db99a3db7a8e7c58eb567dd0f508812a25d58f
IliaWorobiew <iliaworobiew@gmail.com>
2023-02-25 20:38:33 +0300

    Second Commit Thread Counter Stable

--- app/src/main/java/com/example/myapplication3/MainActivity.java
+++ app/src/main/java/com/example/myapplication3/MainActivity.java
@@ -6,15 +6,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private Boolean is_Start = false;
+    private int counter = 0;
+    private TextView TV1;
+    private Button B1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        TV1 = findViewById(R.id.TV);
+        B1 = findViewById(R.id.button1);
     }
 
     public void onClickStart(View View) {
@@ -22,7 +30,21 @@ public class MainActivity extends AppCompatActivity {
         new Thread(new Runnable() {
             @Override
             public void run() {
-
+                while(is_Start)
+                {
+                    counter++;
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            TV1.setText(String.valueOf(counter));
+                        }
+                    });
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
             }
 
 
